287b9a2326205253afd09e86a1f728e7eff74106
Atomu Handa <atomu.handa@accenture.com>
2026-07-13 14:56:23 +0900

    Add GitHub Releases auto-update source (hayawaza/hayawaza)

--- src/ShortcutOverlay/Program.cs
+++ src/ShortcutOverlay/Program.cs
@@ -1,4 +1,5 @@
 using Velopack;
+using Velopack.Sources;
 
 namespace ShortcutOverlay;
 
@@ -10,6 +11,20 @@ public class Program
         VelopackApp.Build().Run();
 
         var app = new App();
+        app.Startup += async (_, _) => await CheckForUpdatesAsync();
         app.Run();
+    }
+
+    private static async Task CheckForUpdatesAsync()
+    {
+        try
+        {
+            var source = new GithubSource("https://github.com/hayawaza/hayawaza", null, false);
+            var manager = new UpdateManager(source);
+            var newVersion = await manager.CheckForUpdatesAsync();
+            if (newVersion != null)
+                await manager.DownloadUpdatesAsync(newVersion);
+        }
+        catch { }
     }
 }
